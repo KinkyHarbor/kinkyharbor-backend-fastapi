5c4a716285430c01c0a077634c99070b86e31533
Kinky Robbie <61333198+KinkyRobbie@users.noreply.github.com>
2020-02-26 20:53:10 +0100

    Add DEMO mode

--- README.md
+++ README.md
@@ -8,3 +8,18 @@ cd jwt-keys
 openssl ecparam -genkey -name secp521r1 -noout -out private.pem
 openssl ec -in private.pem -pubout -out public.pem 
 ```
+
+## Env variables
+### Types of variables
+<dl>
+  <dt>Boolean</dt>
+  <dd>Default: False</dd>
+  <dd>True (not case sensitive) => True</dd>
+  <dd>Everything else => False</dd>
+</dl>
+
+### Variables
+<dl>
+  <dt>DEMO (Boolean)</dt>
+  <dd>Disable verification for registration, password reset and email changes.</dd>
+</dl>
--- app/core/settings.py
+++ app/core/settings.py
@@ -1,3 +1,5 @@
+'''This module handles all ENV and file based settings.'''
+
 import logging
 import sys
 from os import environ
@@ -7,12 +9,22 @@ import aiofiles
 
 
 def get_required_env(name: str) -> str:
+    '''Return a mandatory variable from ENV.'''
     env_var = environ.get(name)
     if not env_var:
-        logging.error(f'{name} not defined in ENV')
+        logging.error('%s not defined in ENV', env_var)
         sys.exit(1)
     return env_var
 
+
+def get_bool(name: str) -> bool:
+    '''Return an optional bool from ENV.'''
+    value = environ.get(name, 'False')
+    return value.lower() == 'true'
+
+
+# General
+DEMO = get_bool('DEMO')
 
 # JWT settings
 JWT_KEY_PATH = Path('../jwt-keys')
@@ -21,7 +33,9 @@ JWT_ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
 async def get_jwt_key(key: str):
-    async with aiofiles.open(JWT_KEY_PATH / f'{key}.pem', mode='r') as f:
+    '''Return ECDSA keys from files for JWT signing.'''
+    filename = f'{key}.pem'
+    async with aiofiles.open(JWT_KEY_PATH / filename, mode='r') as f:
         return await f.read()
 
 # Mongo settings
