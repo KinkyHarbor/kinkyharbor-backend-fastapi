61a8a236ce1f8748599631c34608c739fc8d4539
Kinky Robbie <61333198+KinkyRobbie@users.noreply.github.com>
2020-02-26 21:37:10 +0100

    Redirect root to docs

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@
 
 import uvicorn
 from fastapi import FastAPI
+from starlette.responses import RedirectResponse
 
 from routers import auth, users
 
@@ -15,6 +16,13 @@ app.include_router(
     prefix='/users',
     tags=['users']
 )
+
+
+@app.get('/', include_in_schema=False)
+async def redirect_to_docs():
+    '''Redirect root page to docs.'''
+    return RedirectResponse(url='/docs')
+
 
 # Allows debugging of the application
 # https://fastapi.tiangolo.com/tutorial/debugging/
