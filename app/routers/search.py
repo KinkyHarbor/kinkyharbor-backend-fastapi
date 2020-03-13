d1f5f06924ce7a6aef47844975b3ca939b14fa00
Kinky Robbie <61333198+KinkyRobbie@users.noreply.github.com>
2020-03-13 13:53:12 +0100

    Return empty lists when nothing found

--- app/routers/search.py
+++ app/routers/search.py
@@ -1,7 +1,5 @@
 '''This module handles all routes for search operations'''
 
-import logging
-
 from fastapi import APIRouter, Depends
 from motor.motor_asyncio import AsyncIOMotorDatabase as MotorDB
 
@@ -29,5 +27,8 @@ async def search(q: str,
         user_list = []
 
     return {
-        'users': user_list
+        'users': user_list,
+        'groups': [],
+        'pages': [],
+        'events': [],
     }
