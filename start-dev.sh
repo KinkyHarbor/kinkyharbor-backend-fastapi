0b94a56985b2f7a75c02d45529fe9ca20001dacc
Kinky Robbie <61333198+KinkyRobbie@users.noreply.github.com>
2020-03-21 22:22:14 +0100

    Simplify dev script

--- start-dev.sh
+++ start-dev.sh
@@ -6,11 +6,6 @@ docker-compose -f docker-compose.dev.yml up -d
 echo -e "\nActivate Python env ..."
 source env/bin/activate
 
-echo -e "\nOpen sub shell ..."
-/usr/bin/env bash -c '
-echo -e "\nChange to app directory ..."
-cd app
-
 echo -e "\nStart uvicorn dev server ..."
 FRONTEND_URL=http://192.168.20.50:3000 \
 EMAIL_FROM_ADDRESS=no-reply@kinkyharbor.com \
@@ -18,4 +13,3 @@ EMAIL_SECURITY=unsecure \
 uvicorn harbor.app:app \
 --reload \
 --host=0.0.0.0
-'
