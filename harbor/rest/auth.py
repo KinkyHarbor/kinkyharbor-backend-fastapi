8970f743dbeb0de904caba3dea822a4cbf52f409
Kinky Robbie <61333198+KinkyRobbie@users.noreply.github.com>
2020-03-22 20:57:48 +0100

    Add password reset usecase

--- harbor/rest/auth.py
+++ harbor/rest/auth.py
@@ -4,29 +4,20 @@ from starlette.responses import JSONResponse
 from starlette.status import HTTP_400_BAD_REQUEST, HTTP_401_UNAUTHORIZED, HTTP_409_CONFLICT
 from fastapi import APIRouter, Depends, HTTPException, BackgroundTasks
 from fastapi.security import OAuth2PasswordRequestForm
-from motor.motor_asyncio import AsyncIOMotorDatabase as MotorDB
 from pydantic import BaseModel, Field, constr
 
-from harbor.core import auth, email
 from harbor.domain.common import ObjectIdStr, StrongPasswordStr, Message
 from harbor.domain.email import EmailAddress
-from harbor.domain.token import (
-    AccessToken,
-    AccessRefreshTokens,
-    RefreshToken,
-    VerificationTokenRequest as VerifTokenReq,
-    VerificationPurposeEnum as VerifPur,
-)
-from harbor.domain.user import UserFlags
+from harbor.domain.token import AccessToken, AccessRefreshTokens
+from harbor.repository.base import RepoDict, get_repos
 from harbor.use_cases.user import (
     login as uc_user_login,
     register as uc_user_register,
     register_verify as uc_user_reg_verify,
+    reset_password_exec as uc_user_reset_pw_exec,
+    reset_password_req as uc_user_reset_pw_req,
     token_refresh as uc_token_refresh,
 )
-from harbor.repository.base import RepoDict, get_repos
-from harbor.repository.mongo import users, verif_tokens, refresh_tokens
-from harbor.repository.mongo.common import get_db
 
 
 router = APIRouter()
@@ -179,15 +170,17 @@ async def login_for_access_token(creds: OAuth2PasswordRequestForm = Depends(),
 @router.post("/login/request-password-reset/", response_model=Message)
 async def request_password_reset(address: EmailAddress,
                                  background_tasks: BackgroundTasks,
-                                 db: MotorDB = Depends(get_db)):
-    user = await users.get_login(db, address.email)
-    if user:
-        # User found => Send verification token
-        token = await verif_tokens.create_verif_token(db, user.id, VerifPur.RESET_PASSWORD)
-        recipient = email.get_address(user.username, user.email)
-        msg = email.prepare_reset_password(
-            recipient, token.user_id, token.secret)
-        background_tasks.add_task(email.send_mail, msg)
+                                 repos: RepoDict = Depends(get_repos)):
+    '''User requests a password reset'''
+    uc = uc_user_reset_pw_req.RequestPasswordResetUseCase(
+        user_repo=repos['user'],
+        vt_repo=repos['verif_token'],
+        background_tasks=background_tasks,
+    )
+    uc_req = uc_user_reset_pw_req.RequestPasswordResetRequest(
+        email=address
+    )
+    await uc.execute(uc_req)
     return {'msg': 'Verification mail sent, if email is linked to an existing user'}
 
 
@@ -200,22 +193,25 @@ class PasswordResetBody(BaseModel):
 
 @router.post("/login/password-reset/", response_model=Message)
 async def password_reset(body: PasswordResetBody,
-                         db: MotorDB = Depends(get_db)):
-    token = VerifTokenReq(secret=body.token,
-                          user_id=body.user_id,
-                          purpose=VerifPur.RESET_PASSWORD)
-    valid = await verif_tokens.verify_verif_token(db, token)
-    if not valid:
-        raise HTTPException(
+                         repos: RepoDict = Depends(get_repos)):
+    '''Verifies password reset token and sets new password'''
+    uc = uc_user_reset_pw_exec.ExecResetPasswordUseCase(
+        user_repo=repos['user'],
+        vt_repo=repos['verif_token'],
+    )
+    uc_req = uc_user_reset_pw_exec.ExecResetPasswordRequest(
+        **body
+    )
+
+    try:
+        result = uc.execute(uc_req)
+        if result == uc_user_reset_pw_exec.ExecResetPasswordResponse.UPDATED:
+            return {'msg': 'Password is updated'}
+        elif result == uc_user_reset_pw_exec.ExecResetPasswordResponse.UPDATED_AND_VERIFIED:
+            return {'msg': 'Password is updated and account is verified'}
+
+    except uc_user_reset_pw_exec.InvalidTokenError:
+        return JSONResponse(
             status_code=HTTP_401_UNAUTHORIZED,
-            detail="Provided token is not valid"
+            content={'msg': 'Provided token is not valid'}
         )
-
-    # Set new password
-    user = await users.set_password(db, valid.user_id, body.password)
-
-    # Mark account as verified
-    if not user.is_verified:
-        await users.set_flag(db, valid.user_id, UserFlags.VERIFIED, True)
-        return {'msg': 'Password is updated and account is verified'}
-    return {'msg': 'Password is updated'}
---
+++ harbor/use_cases/user/reset_password_exec.py
@@ -0,0 +1,60 @@
+'''User wants to reset his password'''
+
+import enum
+
+from pydantic import BaseModel
+
+from harbor.core import auth
+from harbor.domain.common import ObjectIdStr, StrongPasswordStr, Message
+from harbor.domain.token import VerificationToken as VerifTokenReq
+from harbor.domain.token import VerificationPurposeEnum as VerifPur
+from harbor.domain.user import UserFlags
+from harbor.repository.base import UserRepo, VerifTokenRepo
+
+
+class ExecResetPasswordRequest(BaseModel):
+    '''Request for verify registration usecase'''
+    user_id: ObjectIdStr
+    token: str
+    password: StrongPasswordStr
+
+
+class ExecResetPasswordResponse(enum.Enum):
+    '''Response for verify registration usecase'''
+    UPDATED: 'passwordUpdated'
+    UPDATED_AND_VERIFIED: 'passwordUpdatedAndAccountVerified'
+
+
+class InvalidTokenError(Exception):
+    '''Provided token is invalid'''
+
+
+class ExecResetPasswordUseCase:
+    '''User wants to reset his password'''
+
+    def __init__(self, user_repo: UserRepo, vt_repo: VerifTokenRepo):
+        self.user_repo = user_repo
+        self.vt_repo = vt_repo
+
+    async def execute(self, req: ExecResetPasswordRequest) -> Message:
+        '''Exchanges token for right to set new password and sets VERIFIED flag on user account
+
+        Raises:
+            InvalidTokenError: Provided token is invalid
+        '''
+        token = VerifTokenReq(secret=req.token,
+                              user_id=req.user_id,
+                              purpose=VerifPur.RESET_PASSWORD)
+        valid = await self.vt_repo.verify_verif_token(token)
+        if not valid:
+            raise InvalidTokenError
+
+        # Set new password
+        password_hash = auth.get_password_hash(req.password)
+        user = await self.user_repo.set_password(valid.user_id, password_hash)
+
+        # Mark account as verified
+        if not user.is_verified:
+            await self.user_repo.set_flag(valid.user_id, UserFlags.VERIFIED, True)
+            return ExecResetPasswordResponse.UPDATED_AND_VERIFIED
+        return ExecResetPasswordResponse.UPDATED
