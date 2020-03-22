706df67ced105dee41ddae4cc70080e735acb11c
Kinky Robbie <61333198+KinkyRobbie@users.noreply.github.com>
2020-03-22 20:17:21 +0100

    Add User TokenRefresh usecase

--- harbor/rest/auth.py
+++ harbor/rest/auth.py
@@ -22,6 +22,7 @@ from harbor.use_cases.user import (
     login as uc_user_login,
     register as uc_user_register,
     register_verify as uc_user_reg_verify,
+    token_refresh as uc_token_refresh,
 )
 from harbor.repository.base import RepoDict, get_repos
 from harbor.repository.mongo import users, verif_tokens, refresh_tokens
@@ -125,51 +126,54 @@ class ReplaceRefreshToken(BaseModel):
 
 
 @router.post('/refresh/', response_model=AccessRefreshTokens, responses={401: {"model": Message}})
-async def refresh(req: ReplaceRefreshToken, db: MotorDB = Depends(get_db)):
+async def refresh(req: ReplaceRefreshToken, repos: RepoDict = Depends(get_repos)):
     '''Trades a refresh token to a new access and refresh token (custom implementation)'''
-    (user_id, token) = req.refresh_token.split(':')
-    old_ref_token = RefreshToken(secret=token, user_id=user_id)
-    new_ref_token = await refresh_tokens.replace_token(db, old_ref_token)
-
-    if new_ref_token:
-        access_token = await auth.create_access_token(data={"sub": f'user:{user_id}'})
-        return AccessRefreshTokens(
-            access_token=access_token,
-            refresh_token=f'{user_id}:{new_ref_token.secret}',
-        )
-
-    return JSONResponse(
-        status_code=401,
-        content={'msg': 'Invalid refresh token'}
+    uc = uc_token_refresh.TokenRefreshUseCase(rt_repo=repos['refresh_token'])
+    uc_req = uc_token_refresh.TokenRefreshRequest(
+        refresh_token=req.refresh_token
     )
+
+    try:
+        tokens = await uc.execute(uc_req)
+        return tokens
+
+    except uc_token_refresh.InvalidTokenError:
+        return JSONResponse(
+            status_code=401,
+            content={'msg': 'Invalid refresh token'}
+        )
 
 
 @router.post("/login/token/", summary='oauth2 password grant flow', response_model=AccessToken)
-async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(),
-                                 db: MotorDB = Depends(get_db)):
+async def login_for_access_token(creds: OAuth2PasswordRequestForm = Depends(),
+                                 repos: RepoDict = Depends(get_repos)):
     '''Trades username and password for an access token (oauth2: password grant)'''
     try:
-        user = await auth.authenticate_user(db, form_data.username, form_data.password)
-    except auth.InvalidCredsError:
+        uc = uc_user_login.LoginUseCase(repos['user'], repos['refresh_token'])
+        uc_req = uc_user_login.LoginRequest(
+            login=creds.username,
+            password=creds.password
+        )
+        tokens = await uc.execute(uc_req)
+        return AccessToken(
+            token=tokens.access_token,
+            access_token=tokens.access_token,
+            token_type="bearer"
+        )
+
+    except uc_user_login.InvalidCredsError:
         raise HTTPException(
             status_code=HTTP_401_UNAUTHORIZED,
             detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
-    except auth.UserLockedError:
+
+    except uc_user_login.UserLockedError:
         raise HTTPException(
             status_code=HTTP_401_UNAUTHORIZED,
             detail="User is locked",
             headers={"WWW-Authenticate": "Bearer"},
         )
-
-    # Authentication successful
-    access_token = await auth.create_access_token(data={"sub": f'user:{user.id}'})
-    return AccessToken(
-        token=access_token,
-        access_token=access_token,
-        token_type="bearer"
-    )
 
 
 @router.post("/login/request-password-reset/", response_model=Message)
--- harbor/use_cases/user/verify.py
+++
@@ -1,2 +0,0 @@
-if not isinstance(flag, UserFlags):
-    raise ValueError(f'"{str(flag)}" is not a valid user flag')
