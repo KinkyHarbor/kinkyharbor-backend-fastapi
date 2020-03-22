b2cd1c6335d783b5860074111bfe001b093f0009
Kinky Robbie <61333198+KinkyRobbie@users.noreply.github.com>
2020-03-22 21:25:03 +0100

    Clarify username should not be case sensitive

--- tests/use_cases/user/test_login.py
+++ tests/use_cases/user/test_login.py
@@ -33,7 +33,7 @@ def fixture_test_user():
 
 @pytest.mark.asyncio
 @mock.patch('harbor.core.auth.create_access_token')
-async def test_uc_user_login_success(create_access_token, uc_req, test_user):
+async def test_uc_user_login_case_insensitive_success(create_access_token, uc_req, test_user):
     '''Should return an access and refresh token'''
     # Create mocks
     user_repo = mock.Mock(UserRepo)
