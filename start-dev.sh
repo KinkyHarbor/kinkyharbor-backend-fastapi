4befb5392f0c3f3b524a56db0ae7d1c3d0f7a4fc
Kinky Robbie <61333198+KinkyRobbie@users.noreply.github.com>
2020-04-04 21:34:02 +0200

    Install requirements before starting dev

--- start-dev.sh
+++ start-dev.sh
@@ -6,6 +6,9 @@ docker-compose -f docker-compose.dev.yml up -d
 echo -e "\nActivate Python env ..."
 source env/bin/activate
 
+echo -e "\nInstall requirements"
+pip install -r requirements.txt
+
 echo -e "\nStart uvicorn dev server ..."
 FRONTEND_URL=http://192.168.20.50:3000 \
 EMAIL_FROM_ADDRESS=no-reply@kinkyharbor.com \
