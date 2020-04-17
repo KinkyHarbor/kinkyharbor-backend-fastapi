69f381238eac4a5aad76b08efe298e30d17c85da
Kinky Robbie <61333198+KinkyRobbie@users.noreply.github.com>
2020-04-17 15:48:28 +0200

    Move settings to Pydantic BaseSettings

--- start-dev.sh
+++ start-dev.sh
@@ -10,9 +10,6 @@ echo -e "\nInstall requirements"
 pip install -r requirements.txt
 
 echo -e "\nStart uvicorn dev server ..."
-FRONTEND_URL=http://localhost:3000 \
-EMAIL_FROM_ADDRESS=no-reply@kinkyharbor.com \
-EMAIL_SECURITY=unsecure \
 uvicorn harbor.app:app \
 --reload \
 --host=localhost \
